94e3f85e1eb5f45e362588f372ac93cbb4f85a49
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-03-10 20:43:15 -0700

    basic groundwork is in, ready to begin work on the challenge system

--- games_bot.py
+++ games_bot.py
@@ -19,14 +19,20 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+selected_game = dict() # Store active games in a dictionary
+# selected_game[user_id] will be the game the bot is currently expecting a move for
+
 # Bot commands
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     
+    if not message.content.startswith("$"):
+        return
+    
     if message.content.startswith("$"):
-        await message.channel.send(f"Hello {message.author}")
+        await message.channel.send(f"Hello {message.author.display_name}")
 
 
 client.run(os.environ['DISCORD_BOT_TOKEN'])
