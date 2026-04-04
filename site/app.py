77bfad1b7911107e65b2474c24197f08ba918421
Natalie Chapman <natalie.c.chapman628@gmail.com>
2026-04-04 05:01:00 -0700

    site pages are done

--- site/app.py
+++ site/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+PAGES = [
+    ["/", "Main"],
+    ["/add_bot", "Add the bot to your server"],
+    ["/commands", "Bot Commands and game rules"]
+]
+
 @app.route("/")
 def index():
-    return "index"
+    return render_template("promo.html", pages=PAGES, current_page="/")
 
 @app.route("/add_bot")
 def add_bot_page():
-    return "add_bot_page"
+    return render_template("add_bot.html", pages=PAGES, current_page="/add_bot")
+
+@app.route("/commands")
+def commands_page():
+    return render_template("commands.html", pages=PAGES, current_page="/commands")
